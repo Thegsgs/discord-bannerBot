41c7c7712a79aaeadcb1036dc91d63097cf18784
Simon Sherbinin <simonshrb@gmail.com>
2022-03-03 13:01:08 +0200

    0.8 help menu text change

--- commands/helpMenu.js
+++ commands/helpMenu.js
@@ -8,13 +8,13 @@ const helpMenu = async (client, interaction, embedNum) => {
     .addFields({
       name: "<:binoculars:948678441673314304> General overview:",
       value: `To view your current banner settings, to set up the bot for the first time and to start editing your new banner, please run the /setup command.
-    You will be then presented with a menu. 
-    At the top of the menu you will see your current banner settings.
-    If this is your first time running the bot the settings will be the default settings.
-    Under your current settings you will see the current image of you banner. \n
-    The banner image will change after each change you make to it using the menu.
-    The banner design will not apply until you have ran the start updating command. \n
-    \u200b`,
+                You will be then presented with a menu. 
+                At the top of the menu you will see your current banner settings.
+                If this is your first time running the bot the settings will be the default settings.
+                Under your current settings you will see the current image of you banner. \n
+                The banner image will update after each change you make to it using the menu.
+                The banner design will not apply until you have ran the /start-updating command. \n
+                \u200b`,
     })
     .setFooter("Page 1/4");
 
@@ -25,26 +25,26 @@ const helpMenu = async (client, interaction, embedNum) => {
       {
         name: `<:start:948676054665551873> How to start customizing the banner: \n`,
         value: `To start customizing the banner open the /setup menu and click on the "Customization options" drop down menu.
-  You will be presented with a menu of possible customizatin choices. \n
-  \u200b`,
+                You will be presented with a menu of possible customization options. \n
+                 \u200b`,
       },
       {
         name: "<:backgrounds:939868806166224956> Change background:",
-        value: ` Copy and paste any image you want from the web or copy and paste it's url. 
-    Note the url needs to end with either .png or .jpg, copying and pasting an image if you're having issues using it's link.
-    Note that if the image aspect ratio doesn't fit the discord banner the bot will stretch the image to fit it so the result might be different from what you expected.
-    In case this occurs  looking for the same image in a different aspect ratio or cropping it yourself with an image editing tool. \n
-    \u200b`,
+        value: `Copy and paste any image you want from the internet or paste and send its url. 
+                Note, the url needs to end with either .png or .jpg, if you're having issues using a link, paste and send the image in the chat instead. \n
+                Note, if the image aspect ratio doesn't fit the discord banner the bot will stretch it to fit so the result might be different from what you expected.
+                In case this happens, looking for the same image in a different aspect ratio or cropping it yourself with an image editing tool should solve the issue. \n
+                 \u200b`,
       },
       {
         name: "<:shapes:939868744363163688> Change shape:",
         value: `After choosing this option you will be presented with a list of basic shapes and an option to set your own custom shape.
-    Custom frame shape setting is done by using a backgroundless .png image. If you posted a .png image and the shape  turns out to be a square or rectangle that means the picture you used had a background and was not a try .png image. \n
-    \u200b`,
+                Custom frame shape setting is done by using a backgroundless .png image. If you posted a .png image and the shape on the banner turns out to be a square or rectangle that means the picture you used had a background. \n
+                \u200b`,
       },
       {
         name: "<:fonts:939868716026429530> Change font:",
-        value: `Pick a font from a list, all fonts are taken from google fonts so you can go there and preview the fonts before picking. \n
+        value: `Pick a font from a list, all fonts are taken from google fonts so feel free to go there and preview the fonts before picking. \n
         \u200b`,
       }
     )
@@ -56,7 +56,7 @@ const helpMenu = async (client, interaction, embedNum) => {
     .addFields(
       {
         name: "<:size:942837778859823114> Text size:",
-        value: `Resize your you numbers if you have a very large number of members or if the frame shape you chose clips the numbers on the edges. \n
+        value: `You can Resize the numbers for whatever reason to fit the frame you chose. \n
         \u200b`,
       },
       {
@@ -66,16 +66,16 @@ const helpMenu = async (client, interaction, embedNum) => {
       },
       {
         name: "<:colors:939868880757743627> Colors menu:",
-        value: `Presents a menu to change the colors of different parts of the banner. Colors are represented in HEX, if you're unfamiliar with HEX colors you can read about it online.
-    Typing colors by their names e.g: "Red", "Green" will not work.
-    Additionaly in the frame backgrond color option, you can write "custom" instead of a color to set an image as the frame background instead.
-  Setting an image is the same as setting a background image for the whole banner (See point 1). \n
+        value: `Presents a menu to change colors of different parts of the banner. Colors are represented in HEX, if you're unfamiliar with HEX colors you can read about it [here](https://www.color-hex.com/).
+                Typing colors by their names e.g: "Red" or "Green" will not work. \n
+                Additionaly in the frame background color option, you can write "custom" instead of a color HEX to set an image as the frame background.
+                Setting an image is the same as setting a background image for the whole banner (See "Change background"). \n
   \u200b`,
       },
       {
         name: "<:icons:939868685508685844> Change icons:",
-        value: `Set a different image for any one of the icons. Setting images works the same way as for setting a background. (see 1 and 6.1).
-    Note that it's better to use very small images otherwise the icon can stretch and look unlike it's original image. \n
+        value: `Set a different image for any one of the icons. Setting images works the same way as for setting a background. (See "Change background").
+                Note that it's better to use very small images otherwise the icons can stretch and look unlike its original image. Also using non .png images can result in icons having a background (See "Colors menu"). \n
     \u200b`,
       }
     )
